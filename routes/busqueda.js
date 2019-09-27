3fa1d1ef5aa2600fac5a2df3b19eac70d3e84040
GermanKuchen <germankuchen@gmail.com>
2019-09-27 16:17:13 -0300

    last

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -14,19 +14,34 @@ app.get('/todo/:param', (req, res, next) => {
     var parametro = req.params.param;
     var regex = new RegExp(parametro, 'i');
 
-    Hospital.find({ nombre: regex }, (error, hospitales) => {
-        if (error) {
-            res.status(500).json({ mensaje: 'Se jodio la BD' });
-            return;
-        }
-        res.status(200).json({
-            mensaje: "esta buscando: " + parametro,
-            hospitales: hospitales
+    buscarHospitales(regex).then((retorna) => {
+        res.status(200).json({ hospitales: retorna });
+    }).catch((error) => {
+        res.status(500).json({ mensaje: error });
+    });
+});
+
+function buscarHospitales(filtro) {
+    return new Promise((resolve, reject) => {
+        Hospital.find({ nombre: filtro }, (error, hospitales) => {
+            if (error) {
+                reject('Hubo un problema al buscar en hospitales');
+            }
+            resolve(hospitales);
         });
     });
+}
 
-
-});
+function buscarMedicos(filtro) {
+    return new Promise((resolve, reject) => {
+        Medico.find({ nombre: filtro }, (error, medicos) => {
+            if (error) {
+                reject('Hubo un problema al buscar en medicos');
+            }
+            resolve(medicos);
+        });
+    });
+}
 
 
 module.exports = app;
