6bcef4966735b204a875bedf26a66d9e885219eb
GermanKuchen <germankuchen@gmail.com>
2019-10-07 11:03:57 -0300

    last

--- routes/imagen.js
+++ routes/imagen.js
@@ -3,6 +3,9 @@ var express = require('express');
 
 // Inicializar variables
 var app = express();
+const path = require('path');
+const fs = require('fs');
+
 var Medico = require('../models/medicoModel');
 
 
@@ -14,8 +17,14 @@ app.get('/:coleccion/:nombreImagen', (req, res, next) => {
     var coleccion = req.params.coleccion;
     var nombreImagen = req.params.nombreImagen;
 
+    var url = path.resolve(__dirname, '../uploads/' + coleccion + '/' + nombreImagen);
 
-    res.status(200).json({ coleccion: coleccion, imagen: nombreImagen });
+    if (fs.existsSync(url)) {
+        res.sendFile(url);
+    } else {
+        var url2 = path.resolve(__dirname, '../assets/img/no-img.jpg');
+        res.sendFile(url2);
+    }
 
 });
 
